ea07fcb10911e592ef8f288e071ff3826abc9bd4
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-10-20 12:25:03 +0530

    .

---
+++ fun.js
@@ -0,0 +1,81 @@
+"use strict"; //treat all JS code as newer version
+/*
+let
+const
+var
+
+
+ES6 : 
+prefer not to use var : 
+because of issue in block scoper and functional scope
+*/
+
+// const accountNumber = 2587456;
+
+// let accountEmail = "bhavesh@gmail.com";
+
+// var accountPassword = "bhavesh";
+
+// accountCity = "Anand";
+
+
+// let accountState;
+
+// accountEmail = "bhavesh@live.in";
+
+// accountPassword = "admin";
+
+// accountCity = "Vadodara";
+
+// accountNumber = 245741; //Assignment to constant variable
+
+
+// console.log(accountNumber);
+
+
+// console.table([accountNumber, accountEmail,accountPassword, accountCity, accountState]);
+
+// console.log(25 +25);
+
+// console.log("Bhavesh");
+
+
+// let name = "Bhavesh"; //string
+// let age = 25; //number
+// let isAdmin = false; //boolean
+// let state; //undefined
+
+// console.log(typeof null); //object
+
+let myname = "bhavesh";
+
+console.log(typeof(myname));
+
+let myAge = 25;
+
+console.log(typeof myAge);
+
+let isAdmin = "bhavesh";
+
+let booleanIsLoggedIn = Boolean(isAdmin);
+
+console.log(booleanIsLoggedIn);
+
+//Boolean : 1 => true, 0 => false
+
+let myNumber = 257;
+console.log(typeof myNumber);
+
+let stringNumber = String(myNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+// echo "# program" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/hiersonusingh/program.git
+// git push -u origin main
